3d619720bdf39c2b174b044ffcf25e7e061cc4c9
arockusa <51435595+arockusa@users.noreply.github.com>
2019-06-06 15:28:38 +0800

    改成對的
    改成對的

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //沒事不要亂改
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
